20e4afb2cfc7f18dcbf667e4c07716437fed20d5
root <root@vl850-s-1-162-dhcp.csuchico.edu>
2020-05-07 15:44:13 -0700

    more tests added

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -38,5 +38,48 @@ TEST(PiezasTest, drop_piece_o)
 	ASSERT_EQ(game.dropPiece(0), O);
 }
 
+TEST(PiezasTest, drop_piece_lower_bounds)
+{
+	Piezas game;
+	ASSERT_EQ(game.dropPiece(-1), Invalid);
+}
+
+TEST(PiezasTest, drop_piece_higher_bounds)
+{
+	Piezas game;
+	ASSERT_EQ(game.dropPiece(99), Invalid);
+}
+
+TEST(PiezasTest, drop_piece_full)
+{
+	Piezas game;
+	game.dropPiece(0);
+	game.dropPiece(0);
+	game.dropPiece(0);
+	ASSERT_EQ(game.dropPiece(0), Blank);
+}
+
+TEST(PiezasTest, piece_at_x)
+{
+	Piezas game;
+	game.dropPiece(0);	
+	ASSERT_EQ(game.pieceAt(0,0), X);
+}
+
+TEST(PiezasTest, piece_at_y)
+{
+	Piezas game;
+	game.dropPiece(0);	
+	game.dropPiece(0);	
+	ASSERT_EQ(game.pieceAt(0,1), Y);
+}
+
+TEST(PiezasTest, piece_at_outside_bounds)
+{
+	Piezas game;
+	ASSERT_EQ(game.pieceAt(-1,-1), Invalid);
+}
+
+
 
 
