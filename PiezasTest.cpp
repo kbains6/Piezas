ec5824971fd6f3175222c66053267bcf31faea51
root <root@vl850-s-1-162-dhcp.csuchico.edu>
2020-05-07 23:38:54 -0700

    done

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -93,26 +93,30 @@ TEST(PiezasTest, reset_board)
 TEST(PiezasTest, x_win_game)
 {
 	Piezas game;
-	game.dropPiece(0); // x
-	game.dropPiece(1);
-	game.dropPiece(0); // x
-	game.dropPiece(1);
-	game.dropPiece(0); // x
-	game.dropPiece(0); 
-	game.dropPiece(1); // x
-	game.dropPiece(0); 
-	game.dropPiece(2);
-	game.dropPiece(0); 
-	game.dropPiece(2);
-	game.dropPiece(0); 
-	game.dropPiece(3);
-	game.dropPiece(0); 
-	game.dropPiece(3);
-	game.dropPiece(0); 
-	game.dropPiece(3);
 	game.dropPiece(0);
-	game.dropPiece(1);
+	game.dropPiece(5);
 	game.dropPiece(0);
+	game.dropPiece(5);
+	game.dropPiece(0);
+	game.dropPiece(5);
+	game.dropPiece(1);
+	game.dropPiece(5);
+	game.dropPiece(1);
+	game.dropPiece(5);
+	game.dropPiece(1);
+	game.dropPiece(5);
+	game.dropPiece(2);
+	game.dropPiece(5);
+	game.dropPiece(2);
+	game.dropPiece(5);
+	game.dropPiece(2);
+	game.dropPiece(5);
+	game.dropPiece(3);
+	game.dropPiece(5);
+	game.dropPiece(3);
+	game.dropPiece(5);
+	game.dropPiece(3);
+	game.dropPiece(5);
 	ASSERT_EQ(game.gameState(), X);
 }
 
