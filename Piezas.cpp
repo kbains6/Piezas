1627d8d9899f9fff52ce5f534c080f728c1ecc35
root <root@vl850-s-1-162-dhcp.csuchico.edu>
2020-05-07 22:14:09 -0700

    trying

--- Piezas.cpp
+++ Piezas.cpp
@@ -13,20 +13,19 @@
  * [0,0][0,1][0,2][0,3]
  * So that a piece dropped in column 2 should take [0,2] and the next one
  * dropped in column 2 should take [1,2].
-**/
+ **/
 
 
 /**
  * Constructor sets an empty board (default 3 rows, 4 columns) and 
  * specifies it is X's turn first
-**/
+ **/
 Piezas::Piezas()
-{
+{	
 	board.resize(BOARD_ROWS);
 	for(int i = 0; i < BOARD_ROWS; i++){
 		board[i].resize(BOARD_COLS);
 	}
-
 	for(int i = 0; i < BOARD_ROWS; i++){
 		for(int k = 0; k < BOARD_COLS; k++){
 			board[i][k] = Blank;
@@ -38,7 +37,7 @@ Piezas::Piezas()
 /**
  * Resets each board location to the Blank Piece value, with a board of the
  * same size as previously specified
-**/
+ **/
 void Piezas::reset()
 {
 	for(int i = 0; i < BOARD_ROWS; i++){
@@ -55,21 +54,20 @@ void Piezas::reset()
  * In that case, placePiece returns Piece Blank value 
  * Out of bounds coordinates return the Piece Invalid value
  * Trying to drop a piece where it cannot be placed loses the player's turn
-**/ 
+ **/ 
 Piece Piezas::dropPiece(int column)
 {
 	if(column < 0 || column > 3){
-		if(turn == X){
-			turn = O;
-		}
-		else if(turn == O){
-			turn = X;
-		}
+
+		if(turn == X)
+			turn = O;	
+		else
+			turn = X;	
 		return Invalid;
 	}
-	for(int i = 0; i < 3; i++){
+	for(int i = 0; i < BOARD_ROWS; i++){
 		if(board[i][column] == Blank){
-			board[i][column] = turn;
+			board[i][column] = turn;	
 			if(turn == X)
 				turn = O;		
 			else
@@ -77,17 +75,19 @@ Piece Piezas::dropPiece(int column)
 			return board[i][column];
 		}
 	}
+
 	if(turn == X)
 		turn = O;	
 	else
-		turn = X;	
+		turn = X;
+
 	return Blank; // full	
 }
 
 /**
  * Returns what piece is at the provided coordinates, or Blank if there
  * are no pieces there, or Invalid if the coordinates are out of bounds
-**/
+ **/
 Piece Piezas::pieceAt(int row, int column)
 {
 	if(row > BOARD_ROWS || row < 0 || column > BOARD_COLS || column < 0){
@@ -104,43 +104,42 @@ Piece Piezas::pieceAt(int row, int column)
  * the most adjacent pieces in a single line. Lines can go either vertically
  * or horizontally. If both X's and O's have the same max number of pieces in a
  * line, it is a tie.
-**/
+ **/
 Piece Piezas::gameState()
 {
 	int x_max, o_max, x_score, o_score = 0;
-	for(int i = 0; i < 3; i++){
+	for(int i = 0; i < BOARD_ROWS; i++){
 		x_score = 0;
 		o_score = 0;
-		for(int k = 0; k < 4; k++){
-			if(board[i][k] == Blank){
-				return Invalid;
-			}
+		for(int k = 0; k < BOARD_COLS; k++){
+			if(board[i][k] == Blank)
+				return Invalid;	
 			else if(board[i][k] == X){
-				x_score++;
+				x_score += 1;
 				if(x_score > x_max)
 					x_max = x_score;			
 				o_score = 0;
 			}
 			else{
-				o_score++;
+				o_score += 1;
 				if(o_score > o_max)
 					o_max = o_score;		
 				x_score = 0;	
 			}
 		}
 	}		
-	for(int i = 0; i < 4; i++){
+	for(int i = 0; i < BOARD_COLS; i++){
 		x_score = 0;
 		o_score = 0;
-		for(int k = 0; k < 3; k++){
+		for(int k = 0; k < BOARD_ROWS; k++){
 			if(board[k][i] == X){
-				x_score++;
+				x_score += 1;
 				if(x_score > x_max)
 					x_max = x_score;		
 				o_score = 0;	
 			}
 			else{
-				o_score++;
+				o_score += 1;
 				if(o_score > o_max)
 					o_max = o_score;		
 				x_score = 0;	
@@ -148,9 +147,9 @@ Piece Piezas::gameState()
 		}
 	}
 	if(o_max > x_max)
-		return O;	
+		return O;		
 	else if(x_max > o_max)
-		return X;	
+		return X;		
 	else	
-		return Blank;
+		return Blank;	
 }
